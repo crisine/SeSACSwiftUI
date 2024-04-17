72546184e6f4dbea66c3c1bed93d8295b9c72e1e
crisine <crisine@knou.ac.kr>
2024-04-17 23:30:58 +0900

    init: init project

---
+++ SeSACSwiftUI/SeSACSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeSACSwiftUIApp.swift
+//  SeSACSwiftUI
+//
+//  Created by Minho on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct SeSACSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
